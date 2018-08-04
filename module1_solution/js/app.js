0c2e69990d7a50a3a0cae98c8aad8834e13fd699
DESKTOP-U0MVH25\Aranel <sohsum@gmail.com>
2018-08-05 01:46:19 +0800

    Enhance app.js
    1. Update code so that there is no hardcoding of the css class in the js code
    2. Update the message css class variable name from messageColorCode to messageClass
    3. Remove/clean up all the console logs.

--- module1_solution/js/app.js
+++ module1_solution/js/app.js
@@ -5,60 +5,48 @@
   .controller('LunchCheckController', LunchCheckController);
 
   function splitString(stringToSplit, separator) {
-    var arrayOfStrings = stringToSplit.split(separator);
-
-    //console.log('The original string is: "' + stringToSplit + '"');
-    //console.log('The separator is: "' + separator + '"');
-    //console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' / '));
-    return arrayOfStrings;
+    return stringToSplit.split(separator);
   }
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
     $scope.lunchItems = "";
     $scope.messageString = "";
-    $scope.messageColorCode = "";
+    $scope.messageClass = "";
 
-    $scope.checkNumberOfDishes = function(separator){
+    $scope.checkNumberOfDishes = function(separator, cssValidInputClassName, cssInvalidInputClassName){/*,
+                                          validNumOfDishMsg, invalidNumOfDishMsg, invalidEntryMsg){*/
 
       // Split the string
       var arrayOfStrings = splitString( $scope.lunchItems, separator);
 
-      // Check the count
-      var msgStr = "";
-      var msgColor = "";
+      // Check the count and make sure that empty strings or strings 
+      // that only contains spaces are not considered as part of the 
+      // item counts
       var itemCount = 0;
-
-      var i;
-      // for (i = 0; i < arrayOfStrings.length; i++) { 
-      //   if (arrayOfStrings[i].length > 0){
-      //     itemCount++;
-      //   }
-      //   console.log("Item value:" + arrayOfStrings[i] + " & Item count = " + itemCount );
-      // }
       angular.forEach(arrayOfStrings, function(value){
-        if (value.length > 0){
+        if (value.trim().length > 0){
           itemCount++;
         }
-        //console.log("Item value:" + value + " & Item count = " + itemCount );
       }); 
 
-      console.log("Item count = " + itemCount );
-
       // update the final message string and color
+      // by deciding on the appropriate html classes to add
+      var msgStr = "";
+      var msgColor = "";
       if(itemCount > 3){
         msgStr = "Too much!";
-        msgColor = "valid_input";
+        msgColor = cssValidInputClassName;
       } else if (itemCount <= 0){
         msgStr = "Please enter data first!!";
-        msgColor = "invalid_input";
+        msgColor = cssInvalidInputClassName;
       } else {
         msgStr = "Enjoy!";
-        msgColor = "valid_input";
+        msgColor = cssValidInputClassName;
       }
 
       $scope.messageString = msgStr;
-      $scope.messageColorCode = msgColor;
+      $scope.messageClass = msgColor;
     };
   }
 })();
