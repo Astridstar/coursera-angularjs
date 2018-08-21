56e6af7dc1e8e259afe17b028385c197250a9bc4
DESKTOP-U0MVH25\Aranel <sohsum@gmail.com>
2018-08-21 19:01:26 +0800

    Include bootstrap styling

--- module2_solution/js/app.js
+++ module2_solution/js/app.js
@@ -31,7 +31,14 @@
   function ShoppingListCheckOffService() {
     var service = this;
 
-    var toBuyItemsList = ["Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter"];
+    // var toBuyItemsList = ["Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter"];
+    var toBuyItemsList = [
+      { quantity: "2 cartons", desc: "Milk"},
+      { quantity: "5", desc: "Donuts"},
+      { quantity: "20 bags", desc: "Cookies"},
+      { quantity: "10 boxes", desc: "Chocolate"},
+      { quantity: "1 jar", desc: "Peanut Butter"}];
+
     var boughtItemList = [];
     var isBoughtItemsEmpty = true;
 
@@ -41,15 +48,15 @@
           throw new Error("Invalid item given (Item Index: " + itemIndex + ").");
         }
 
-        var itemDescription = toBuyItemsList[itemIndex];
-        console.log("Item removed: ", itemDescription);
+        var theBoughtItem = toBuyItemsList[itemIndex];
+        console.log("Item removed: ", theBoughtItem);
 
         // Remove item from the toBuyItemsList
         toBuyItemsList.splice(itemIndex, 1);
 
         // Add item to the boughtItemList
         isBoughtItemsEmpty = false;
-        boughtItemList.push(itemDescription);
+        boughtItemList.push(theBoughtItem);
         console.log("Bought Items are: ", boughtItemList);
         console.log("Flag Value is : ", isBoughtItemsEmpty);
     };
