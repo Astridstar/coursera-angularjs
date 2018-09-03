a32d5ca0ea95eb55a843f713c43ccc0db25e4e6f
DESKTOP-U0MVH25\Aranel <sohsum@gmail.com>
2018-09-03 21:58:14 +0800

    Module 3 Assignment: Working version but not fully.  All matching items are displayed accordingly.

--- module3_solution/js/app.js
+++ module3_solution/js/app.js
@@ -1,115 +1,99 @@
-( function(){
-  'use strict';
+(function () {
+'use strict';
 
-  angular.module('NarrowItDownApp', [])
-  .controller('NarrowItDownController', NarrowItDownController)
-  .service('menuSearchService', MenuSearchService)
-  .directive('foundItems', FoundItemsDirective)
-  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', DisplayContent)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
-// directive
-  function FoundItemsDirective(){
-    var ddo = {
-      restrict: 'E',
-      scope:{
-          found: '<',
-          onRemove: '&'
-      },
-      transclude: true,
-      controller: FoundItemsDirectiveController,
-      controllerAs: 'controller',
-      templateUrl: "loader/itemsloaderindicator.template.html"
-    };
-    return ddo;
+function DisplayContent(){
+  var ddo = {
+    restrict: 'E',
+    scope: {
+      title: '@',
+      onFound: '<',
+      onRemove: '&'
+    },
+    controller: DisplayContentController,
+    controllerAs: 'display',
+    bindToController: true,
+    //link: DisplayContentLink,
+    templateUrl: 'content-template.html'
+  };
+  return ddo;
+} // function DisplayContent()
+
+function DisplayContentController(){
+  var self = this;
+}; // function DisplayContentController()
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var self = this;
+  var userInput = '';
+  var found = [];
+
+  self.getMatchedMenuItems = function(searchTerm){
+    console.log("getMatchedMenuItems triggered");
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+    promise.then(function(response){
+      console.log("Response added to promise: ");
+      console.log(promise.found);
+      self.found = promise.found;
+    });
   };
 
-  function FoundItemsDirectiveController(){
-    var controller = this;
+  self.removeItem = function(index){
+    found.splice(index, 1);
+  };
+} // function NarrowItDownController(MenuSearchService)
 
-    controller.hasFoundItems = function(){
-      // if( (controller.foundItems === undefined) || (controller.foundItems.length <= 0 ){
-      //   return false;
-      // };
-      return true;
-    };
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var self = this;
+
+  self.retrieveMenuItems = function () {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    });
+
+    return response;
   };
 
-// Controller
-  NarrowItDownController.$inject = ['menuSearchService'];
-  function NarrowItDownController(menuSearchService){
-    var controller = this;
-    var found = [];
-    var searchTerm = '';
+  self.getMatchedMenuItems = function(searchTerm){
+    console.log("Service - SearchTerm = " + searchTerm);
 
-    controller.found = menuSearchService.getFoundItems;
-    controller.getMatchedMenuItems = function(searchTerm){
-      controller.found = menuSearchService.getMatchedMenuItems(searchTerm);
-      console.log(this);
-      console.log("Matching items: ");
-      console.log(controller.found);
-    };
+    var promise = self.retrieveMenuItems();
 
-    controller.removeItem = function(itemIndex){
-      menuSearchService.removeItem(itemIndex);
-    };
+    promise.then(function (response) {
+      //console.log("Unfiltered Response received: ");
+      //console.log(response.data);
+
+      promise.found = self.search(searchTerm, response.data.menu_items);
+      //console.log("Response added to promise: ");
+      //console.log(promise.found);
+    })
+    .catch(function (error) {
+      console.log("Something went terribly wrong.");
+    });
+    return promise;
   };
 
-// Service
-  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
-  function MenuSearchService($http, ApiBasePath){
-    var service = this;
-    var foundItems = [];
-
-    // Async executing of fetching from the server
-    service.getMenuItems = function(){
-      var response = $http({
-        method: "GET",
-        url: (ApiBasePath + "/menu_items.json")
-      });
-      return response;
-    };
-
-    // Modifiers
-    service.removeItem = function (itemIndex){
-      foundItems.splice(itemIndex, 1);
-    };
-
-    service.getFoundItems = function(){
-      return foundItems;
-    };
-
-    service.getMatchedMenuItems = function(searchTerm){
-      console.log("Searching for text: " +  searchTerm.toLowerCase() );
-      var promise = service.getMenuItems();
-
-      service.foundItems = promise.then( function(response, service){
-        var foundItems = [];
-        //console.log("Successful retrieve: ");
-        //console.log(response.data);
-        if(response.data.menu_items === undefined )
-          throw new Error("Menu_items are invalid");
-        // search the results to that matches searchTerm
-        for (var i = 0; i < response.data.menu_items.length; i++) {
-          var item = response.data.menu_items[i];
-          //console.log("Description: "+ item.description.toLowerCase());
-          if(item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)
-            foundItems.push(item);
+  self.search = function(searchTerm, menuItems){
+    //console.log("SearchTerm = " + searchTerm + " & MenuItems = " + menuItems);
+    var matchingItems = [];
+    if( menuItems !== undefined){
+      for(var i = 0; i < menuItems.length; i++){
+        if(menuItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1){
+          matchingItems.push(menuItems[i]);
         }
-        //console.log("Matching items: ");
-        //console.log(foundItems);
-        console.log("No of matching items found: " + foundItems.length);
-        console.log(this);
-        return foundItems;
-      })
-      .catch(function(error) {
-        // Process any error functions
-        console.log("Error caught while executing search: " + error)
-      });
-
-      console.log("In service: getMatchedMenuItems. Found items:---------------");
-      console.log(service.foundItems);
-      console.log(service.foundItems.value);
-    };
-  }; //function MenuSearchService($http, ApiBasePath){
-
+      }
+    }
+    //console.log("Items matching => " + searchTerm);
+    //console.log(matchingItems);
+    return matchingItems;
+  };
+} // function MenuSearchService($http, ApiBasePath)
 })(); // ( function(){ IIFE
